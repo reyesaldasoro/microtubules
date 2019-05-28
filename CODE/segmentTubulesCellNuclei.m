2131736266c22c2aee44c4f01556ef4362590876
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2019-05-28 18:38:57 +0100

    Update segmentTubulesCellNuclei.m
    refine the process to discard cells that do not have nucleus, also fill those that are rather hollow, but not if they are clumps

--- CODE/segmentTubulesCellNuclei.m
+++ CODE/segmentTubulesCellNuclei.m
@@ -17,45 +17,88 @@ level_1                                 = 255* graythresh(channel_1F);
 
 
 % Nuclei
-cellNuclei                              = (channel_1F>(0.75*level_1));
+cellNuclei_1                            = (channel_1F>(0.75*level_1));
+cellNuclei                              = imclose(cellNuclei_1,strel('disk',3));
 
 % CELL BODY
 % Segment by intensity
-cellBody_1F                             = channel_2F>(0.95*level_2);
+cellBody_1                             = channel_2F>(0.95*level_2);
 % Fill in the holes of the cells
-cellBody_2                              = imfill(cellBody_1F| imdilate(cellNuclei,strel('disk',3)),'holes');
+cellBody_2                              = imfill(cellBody_1| imdilate(cellNuclei,strel('disk',3)),'holes');
+
+% Some cells can be rather open, it is important to close the gaps of those
+% cells, but only if: 1) they have only one nucleus (avoid clumps and cells
+% without a nucleus) 2) they are within a range of sizes >400 pixels to
+% avoid small specks 
+
 cellBody_2L                             = bwlabel(cellBody_2);
-cellBody_2P                             = regionprops(cellBody_2L,'Area','ConvexHull','ConvexImage');
+% Remove small specks
+cellBody_2P                             = regionprops(cellBody_2L,'Area');
+cellBody_3                              = ismember(cellBody_2L,find([cellBody_2P.Area]>400));
+cellBody_3L                             = bwlabel(cellBody_3);
 
-cellBody_3                              = ismember(cellBody_2L,find([cellBody_2P.Area]>200));
-cellBody_3P                             = regionprops(cellBody_3,'Area','ConvexHull','ConvexImage','BoundingBox');
+% Remove cells without nuclei
+cellsBody_3L_Nuclei                     = cellBody_3L.*cellNuclei;
+cellsWithNuclei                         = unique(cellsBody_3L_Nuclei);
+cellBody_4                              = ismember(cellBody_3L,cellsWithNuclei(2:end));
+cellBody_4L                             = bwlabel(cellBody_4);
+
+% Determine cells with 2 nuclei (clumps)
+nucleiPresent_centroids                 =(bwmorph(cellNuclei,'shrink','inf'));
+nucleiPresent_location                  = find(nucleiPresent_centroids);
+nucleiPresent                           = cellBody_4L(nucleiPresent_location);
+
+
+
 %%
-cellBody_3R = zeros(rows,cols);
+cellBody_4E                             = imopen(cellBody_4L,strel('disk',3));
+cellBody_4EP                            = regionprops(cellBody_4E,'Area','ConvexHull','ConvexImage','BoundingBox','Solidity','Eccentricity');
+cellBody_4P                             = regionprops(cellBody_4L,'Area','ConvexHull','ConvexImage','BoundingBox','Solidity','Eccentricity');
+% The combination for a hollow cell is that 1) it has a solidity below 0.8
+% 2) it is not too large nor too small ( 700-2000) is not too elongated
+% eccentricity < 0.9 and only one nucleus
+imagesc(cellBody_4E)
 
-for k=1:numel(cellBody_3P)
-%k=17;
-    
-        %disp(k)
-        rr = max(1,ceil(cellBody_3P(k).BoundingBox(2))):min(rows,floor(cellBody_3P(k).BoundingBox(2)+cellBody_3P(k).BoundingBox(4)));
-        cc = max(1,ceil(cellBody_3P(k).BoundingBox(1))):min(cols,floor(cellBody_3P(k).BoundingBox(1)+cellBody_3P(k).BoundingBox(3)));
-        
-        cellBody_3R(rr,cc) = cellBody_3R(rr,cc) + cellBody_3P(k).ConvexImage;
-           
+%%
+cellBody_4R = cellBody_4L;%zeros(rows,cols);
 
-% imagesc(BW5)
+for k=1:numel(cellBody_4EP)
+    %k=17;
+    if (cellBody_4EP(k).Solidity<0.8)
+        if (cellBody_4EP(k).Area<2500)
+            if (cellBody_4EP(k).Area>500)
+                if (cellBody_4EP(k).Eccentricity<0.95)
+                    if (sum(nucleiPresent==k)==1)
+                        disp(k)
+                        rr = max(1,ceil(cellBody_4P(k).BoundingBox(2))):min(rows,floor(cellBody_4P(k).BoundingBox(2)+cellBody_4P(k).BoundingBox(4)));
+                        cc = max(1,ceil(cellBody_4P(k).BoundingBox(1))):min(cols,floor(cellBody_4P(k).BoundingBox(1)+cellBody_4P(k).BoundingBox(3)));
+                        
+                        cellBody_4R(rr,cc) = (cellBody_4L(rr,cc)) + cellBody_4P(k).ConvexImage;
+                        
+                    end
+                    
+                end
+            end
+        end
+    end
 end
+cellBody_5                              = cellBody_4R>0;
+%% Remove stems that are themselves tubules
+cellBody_6                             = imopen(cellBody_5,strel('disk',5));
+
+
 %%
 %cellBody_3                              = imopen(cellBody_2,ones(3));
-cellBody_4                              = bwdist(cellBody_3);
-cellBody_5                              = (cellBody_4)<60;
+distFromCells                              = bwdist(cellBody_6);
+regionCells                              = (distFromCells)<60;
 
-cellBody                                = cellBody_3;
+cellBody                                = cellBody_6;
 
-imagesc(cellBody_3+2*cellNuclei)
-%%
+%imagesc(cellBody+2*cellNuclei)
+%% Prepare for the tubules
 
 BW                                      = edge((uint8(1-cellBody_3).*channel_2),'canny',[],1);
-BW2                                     = cellBody_5.*(BW.*imerode((1-cellBody_3),ones(3)));
+BW2                                     = regionCells.*(BW.*imerode((1-cellBody_3),ones(3)));
 [BW3,numEdges]                          = bwlabel(BW2);
 BW4 = regionprops(BW3,channel_2,'Area',...
     'MajoraxisLength','MinoraxisLength',...
