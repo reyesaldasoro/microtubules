265509a56f8132f1cc9b9c1efc02b67585453bfc
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2019-05-29 15:59:29 +0100

    Update segmentTubules.m
    Final refinement, get the tubules closer to the cells

--- CODE/segmentTubules.m
+++ CODE/segmentTubules.m
@@ -21,7 +21,7 @@ level_1                                 = 255* graythresh(channel_1F);
 %cellBody_3                              = imopen(cellBody_2,ones(3));
 distFromCells                           = bwdist(cellBody);
 regionCells                             = (distFromCells)<55;
-regionEdges                             = regionCells-imdilate(cellBody,ones(5));
+regionEdges                             = regionCells-imdilate(cellBody,ones(3));
 
 %imagesc(cellBody+2*cellNuclei)
 %% Prepare for the tubules
@@ -56,6 +56,7 @@ BW2_endp_b                              = (bwmorph(BW2_endp,'bridge'));
 
 %BW2_endp_3                                = imopen(BW2_endp_2,[0 1 0;1 1 1;0 1 0]);
 
+% Label to discard very small edges
 [BW3,numEdges]                          = bwlabel(BW2|BW2_endp_b);
 BW4 = regionprops(BW3,channel_2,'Area',...
     'MajoraxisLength','MinoraxisLength',...
@@ -63,39 +64,40 @@ BW4 = regionprops(BW3,channel_2,'Area',...
 %imagesc(BW)
 %%
 BW5 = zeros(rows,cols);
-%%
+%% A tubule will show as edges on both sides, so close one by one 
+% so that separate tubules do not connect, especially parallel ones
 for k=1:numEdges
-%k=17;
     if (BW4(k).Area<400)&&(BW4(k).Area>10)
         %disp(k)
         rr = max(1,floor(BW4(k).BoundingBox(2))):min(rows,ceil(BW4(k).BoundingBox(2)+BW4(k).BoundingBox(4)));
         cc = max(1,floor(BW4(k).BoundingBox(1))):min(cols,ceil(BW4(k).BoundingBox(1)+BW4(k).BoundingBox(3)));
-        
         BW5(rr,cc) = BW5(rr,cc) + k*imfill(imclose(BW3(rr,cc)==k,ones(3)),'holes');
-           
     end
 % imagesc(BW5)
 end
-%%
-BW6 = BW5 - BW2;
-BW7 = bwlabel(BW6==1);
+%% Thin the edges and remove spurious pixels
+BW6 = (bwmorph(BW5,'thin','inf'));
+BW7 = bwlabel(bwmorph(BW6,'spur',0));
+
+%BW6 = BW5 - BW2;
+%BW7 = bwlabel(BW6==1);
 
 BW8 = regionprops(BW7,channel_2,'Area',...
     'MajoraxisLength','MinoraxisLength',...
     'Eccentricity','Euler','MaxIntensity','BoundingBox');
 %% Final tubules
 % Brightest tubules
-brightTubLev    = 0.05*max([BW8.MaxIntensity]) +0.95*   mean([BW8.MaxIntensity]);
-BW9A             =(ismember(BW7,find([BW8.MaxIntensity]>brightTubLev))).*(ismember(BW7,find([BW8.Area]>15)));
+%brightTubLev    = 0.05*max([BW8.MaxIntensity]) +0.95*   mean([BW8.MaxIntensity]);
+%BW9A             =(ismember(BW7,find([BW8.MaxIntensity]>brightTubLev))).*(ismember(BW7,find([BW8.Area]>15)));
 % Long and straight
-BW9B             =(ismember(BW7,find([BW8.Eccentricity]>0.96))).*(ismember(BW7,find([BW8.Area]>6)));
+%BW9B             =(ismember(BW7,find([BW8.Eccentricity]>0.96))).*(ismember(BW7,find([BW8.Area]>6)));
 %imagesc((BW7>0)+BW9B+2*BW9A)
 %% Now include those that are 1-2 pixels away of the previously selected
-BW10             = bwdist(BW9A + BW9B);
-BW11            = unique(BW7.*(BW10<3));
-BW12            = ismember(BW7,BW11(2:end));
-BW13            = imclose(BW12,ones(1));
-
+%BW10             = bwdist(BW9A + BW9B);
+%BW11            = unique(BW7.*(BW10<3));
+%BW12            = ismember(BW7,BW11(2:end));
+%BW13            = imclose(BW12,ones(1));
 %imagesc((BW7>0)+(BW13))
-cellTubules         = BW13;
+%cellTubules         = BW13;
+cellTubules         = ismember(BW7,find([BW8.MajorAxisLength]>6));
 
