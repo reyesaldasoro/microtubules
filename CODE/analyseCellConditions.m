03e1470f9c9daa4b0d12e66985bd112286de5135
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2019-06-14 16:21:26 +0100

    Update analyseCellConditions.m
    Besides the erosion is necessary to check if the nuclei are in different partitions.

--- CODE/analyseCellConditions.m
+++ CODE/analyseCellConditions.m
@@ -19,16 +19,26 @@ notClumps                               = setdiff(C,clumps);
 %% Solid Clumps and just touching
 % Further, detect those that are very close and only in contact by a very
 % thin connection, find the degree of the connection
+% The erosion may break a section of the cell but stay as a clump
 numClumps = numel(clumps);
 degreeClump(numClumps) = 0;
 for counterClump = 1:numClumps
-    numCellClump=1;
-    sizeStrel = 1;
-    while numCellClump==1
-        sizeStrel=sizeStrel+1;
-        [~,numCellClump]=bwlabel(imopen(cellBody_L==clumps(counterClump),strel('disk',sizeStrel)));
-        degreeClump(counterClump) = sizeStrel;
+    numCellClump                    = 1;
+    maxLabelNuclei                  = 1;
+    sizeStrel                       = 1;
+%    while numCellClump==1
+    while (maxLabelNuclei<2)&&(sizeStrel<35)
+        sizeStrel                   = sizeStrel+1;
+        [res,numCellClump]          = bwlabel(imerode(cellBody_L==clumps(counterClump),strel('disk',sizeStrel)));
+        %labelNuclei                 = res(nucleiPresent_centroids);
+        labelNuclei                 = unique(res.*cellNuclei);
+        %maxLabelNuclei              = max(labelNuclei);
+        maxLabelNuclei              = -1+numel(labelNuclei);
+        degreeClump(counterClump)   = sizeStrel;
+        %imagesc(res)
+        %drawnow
     end
+%    imagesc(res)
 end
 
 
