d8c953db027318d08bed73df5fa0e7c96fa4184a
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2019-06-21 10:57:43 +0100

    Update allocateTubules.m
    Start allocating the tubules

--- CODE/allocateTubules.m
+++ CODE/allocateTubules.m
@@ -26,4 +26,41 @@ for counterProt = 1:numProt
 end
 %%
 
-imagesc(cellProtrusions_L2)
+%%
+cellBody_L_Complete     = cellProtrusions_L2+cellBody_L;
+cellBody_L_CompleteD    = imdilate(cellBody_L_Complete,ones(9));
+
+%%
+imagesc(cellBody_L_CompleteD.*cellTubules)
+
+%%
+
+[cellTubules_L,numTubules]          = bwlabel(cellTubules);
+cellTubules_L2                      = zeros(size(cellTubules_L));
+
+for counterTub = 1:numProt
+    currentTub                     = (cellTubules_L==counterTub);
+    currentOverlap                  = cellBody_LD.*currentTub;
+    classTub                       = unique(currentOverlap);
+    if numel(classTub)==2
+        % only two elements, [0 X] allocate to the second element
+        cellTubules_L2          = cellTubules_L2 + classTub(2)*currentTub;
+    elseif numel(classTub)>2
+        % more than one element, should allocate to whichever is closest
+        % ... or has more of it 
+        for counterClasses = 1:numel(classTub)-1
+            numElements(counterClasses) = sum(currentOverlap(:)==classTub(counterClasses+1));
+        end
+        [maxNum,indMax]= max(numElements);
+        cellTubules_L2  = cellTubules_L2 + classTub(indMax+1)*currentTub;
+        
+    else
+        % tubule does not overlap, leave at 1 for the time being
+    end
+end
+
+%%
+imagesc(cellBody_L_CompleteD.*cellTubules)
+
+%%
+qq=1;
