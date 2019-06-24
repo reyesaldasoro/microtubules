ad712c07827245ed0399e9dc019e259a44e64cc1
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2019-06-24 12:31:17 +0100

    Update analyseCellConditions.m
    correct the cases where there are no clumps at all!

--- CODE/analyseCellConditions.m
+++ CODE/analyseCellConditions.m
@@ -20,13 +20,15 @@ notClumps                               = setdiff(C,clumps);
 % Further, detect those that are very close and only in contact by a very
 % thin connection, find the degree of the connection
 % The erosion may break a section of the cell but stay as a clump
+if ~isempty(clumps)
 numClumps = numel(clumps);
+ 
 degreeClump(numClumps) = 0;
 for counterClump = 1:numClumps
     numCellClump                    = 1;
     maxLabelNuclei                  = 1;
     sizeStrel                       = 1;
-%    while numCellClump==1
+    %    while numCellClump==1
     while (maxLabelNuclei<2)&&(sizeStrel<35)
         sizeStrel                   = sizeStrel+1;
         [res,numCellClump]          = bwlabel(imerode(cellBody_L==clumps(counterClump),strel('disk',sizeStrel)));
@@ -38,9 +40,12 @@ for counterClump = 1:numClumps
         %imagesc(res)
         %drawnow
     end
-%    imagesc(res)
+    %    imagesc(res)
 end
-
+else
+    degreeClump =[];
+    
+end
 
 
 
